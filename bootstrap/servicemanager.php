51368747c14a7c04d1675b8b4433a98cefe97ccb
Lucas Seinfeld <l.seinfeld@ixolit.com>
2019-04-20 01:42:17 +0200

    fix cs

--- bootstrap/servicemanager.php
+++ bootstrap/servicemanager.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @link https://github.com/ixocreate
+ * @copyright IXOCREATE GmbH
+ * @license MIT License
+ */
+
 declare(strict_types=1);
 
 namespace Ixocreate\CommandBus;
@@ -9,7 +15,6 @@ use Ixocreate\CommandBus\Factory\CommandBusFactory;
 use Ixocreate\CommandBus\Handler\HandlerSubManager;
 
 /** @var ServiceManagerConfigurator $serviceManager */
-
 $serviceManager->addFactory(CommandBus::class, CommandBusFactory::class);
 $serviceManager->addSubManager(CommandSubManager::class);
 $serviceManager->addSubManager(HandlerSubManager::class);
